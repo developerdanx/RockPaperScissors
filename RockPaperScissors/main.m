4bcb4452e62881a77703837c6076bf484f601245
Dan <d.petre@lookiero.com>
2022-04-14 19:20:49 +0200

    Initial Commit

---
+++ RockPaperScissors/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RockPaperScissors
+//
+//  Created by Dan Petre on 14/4/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
